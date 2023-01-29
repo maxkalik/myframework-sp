7cf1df7c7c112af696b2df7fafede5f768f83e19
maxkalik <maxkalik@gmail.com>
2023-01-29 13:38:47 -0800

    package inited

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "MyFramework",
+    products: [
+        .library(
+            name: "MyFramework",
+            targets: ["MyFramework"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/TimOliver/TOCropViewController.git", from: "2.6.1"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.5.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MyFramework",
+            url: "https://cdn.triumpharcade.com/triumph-kit-releases/MyFramework-v1.0.8.zip",
+            checksum: "d6b7967696d2144d5659edc4d24f03135427b6032cb228d61954ea3e052ddf7d"
+        )
+    ]
+)
